e74015c244a57b050f084308a4b02865c120e19b
Yihuan TANG <yihuan.tang1@gmail.com>
2022-04-17 23:27:25 -0700

    some typos and recheck

--- Day3.sql
+++ Day3.sql
@@ -27,37 +27,53 @@ FROM Employees  E RIGHT JOIN Customers C ON E.City = C.City
 WHERE E.City IS NULL
 
 --3. List all products and their total order quantities throughout all orders.
--- if just productID and TotalQuantitySold is required 
-SELECT ProductID, SUM(Quantity) TotalQuantityOrdered
-FROM [Order Details] 
-GROUP BY ProductID
+SELECT P.ProductID, P.ProductName,  SUM(OD.Quantity) TotalQuantityOrdered
+FROM [Order Details]  OD RIGHT JOIN Products P ON P.ProductID = OD.ProductID
+GROUP BY P.ProductID, P.ProductName
+
+-- if we want to sort by TotalQuantityOrdered DESC
+SELECT P.ProductID, P.ProductName,  SUM(OD.Quantity) TotalQuantityOrdered
+FROM [Order Details]  OD RIGHT JOIN Products P ON P.ProductID = OD.ProductID
+GROUP BY P.ProductID, P.ProductName
+ORDER BY TotalQuantityOrdered DESC
+
 
 --4.      List all Customer Cities and total products ordered by that city.
 --
 SELECT C.City, SUM(OD.Quantity) TotalQuantityOrdered
-FROM [Order Details] OD RIGHT JOIN Orders O ON OD.OrderID = O.OrderID INNER JOIN Customers C ON C.CustomerID = O.CustomerID
+FROM [Order Details] OD INNER JOIN Orders O ON OD.OrderID = O.OrderID RIGHT JOIN Customers C ON C.CustomerID = O.CustomerID
 GROUP BY C.City
+
+-- if we want to sort by quantity desc 
+SELECT C.City, SUM(OD.Quantity) TotalQuantityOrdered
+FROM [Order Details] OD INNER JOIN Orders O ON OD.OrderID = O.OrderID RIGHT JOIN Customers C ON C.CustomerID = O.CustomerID
+GROUP BY C.City
+ORDER BY TotalQuantityOrdered DESC
 
 --5.      List all Customer Cities that have at least two customers.
 --direct approach cost: 9%
-SELECT City
+SELECT City, COUNT(CustomerID) TotalNbOfCustomers
 FROM Customers 
 GROUP BY City
 HAVING COUNT(CustomerID) >= 2
 --a.      Use union
 -- except 0 and 1 , union 0 and 1  cost:57%
-SELECT City FROM Customers
+SELECT City, COUNT(CustomerID) TotalNbOfCustomers  FROM Customers
+GROUP BY City
 EXCEPT 
 (
-SELECT City FROM Customers GROUP BY City HAVING COUNT(CustomerID) = 1
+SELECT City, COUNT(CustomerID) TotalNbOfCustomers FROM Customers GROUP BY City HAVING COUNT(CustomerID) = 1
 UNION 
-SELECT City FROM Customers GROUP BY City HAVING COUNT(CustomerID) = 0
+SELECT City , COUNT(CustomerID) TotalNbOfCustomers FROM Customers GROUP BY City HAVING COUNT(CustomerID) = 0
 )
 --b.      Use sub-query and no union cost:34%
-SELECT City FROM Customers
+SELECT City, COUNT(CustomerID) TotalNbOfCustomers 
+FROM Customers
+GROUP BY City
 EXCEPT 
 (
-SELECT City FROM Customers 
+SELECT City , COUNT(CustomerID) TotalNbOfCustomers
+FROM Customers 
 GROUP BY City 
 HAVING COUNT(CustomerID) <2
 )
@@ -74,7 +90,7 @@ HAVING Count(DISTINCT P.CategoryID) >= 2
 
 --7.      List all Customers who have ordered products, but have the ¡®ship city¡¯ on the order different from their own customer cities.
 -- 
-SELECT DISTINCT C.CustomerID
+SELECT DISTINCT C.CustomerID, C.ContactName, C.City, O.ShipCity
 FROM Customers C INNER JOIN Orders O ON C.CustomerID = O.CustomerID
 WHERE C.City != O.ShipCity 
  
@@ -85,19 +101,19 @@ WHERE C.City != O.ShipCity
 WITH PopularTop5 
 AS 
 (
-SELECT TOP 5 ProductID, AVG(UnitPrice) AvgPrice
-FROM [Order Details]
-GROUP BY ProductID
+SELECT TOP 5 P.ProductID, P.ProductNAME, AVG(OD.UnitPrice) AvgPrice
+FROM [Order Details] OD INNER JOIN Products P ON P.ProductID = OD.ProductID
+GROUP BY P.ProductID,  P.ProductNAME
 ORDER BY SUM(Quantity) DESC 
 )
 
-SELECT dt.ProductID, dt.AvgPrice, dt.City, dt.TotalSold
-FROM (SELECT OD.ProductID, P5.AvgPrice, C.City, SUM(OD.Quantity) TotalSold, RANK() OVER (Partition BY OD.ProductID Order by SUM(OD.Quantity) DESC ) AS OrderRank
-FROM Customers C INNER JOIN Orders O ON C.CustomerID = O.CustomerID INNER JOIN [Order Details] OD ON OD.OrderID = O.OrderID INNER JOIN PopularTop5 P5 ON P5.ProductID = OD.ProductID
-GROUP BY OD.ProductID, C.City, P5.AvgPrice) dt
+SELECT dt.ProductID, dt.ProductName,  dt.AvgPrice, dt.City, dt.TotalSold
+FROM 
+(SELECT OD.ProductID, P5.ProductName, P5.AvgPrice, C.City, SUM(OD.Quantity) TotalSold, RANK() OVER (Partition BY OD.ProductID Order by SUM(OD.Quantity) DESC ) AS OrderRank
+FROM Customers C INNER JOIN Orders O ON C.CustomerID = O.CustomerID INNER JOIN [Order Details] OD 
+ON OD.OrderID = O.OrderID INNER JOIN PopularTop5 P5 ON P5.ProductID = OD.ProductID
+GROUP BY OD.ProductID, P5.ProductName, C.City, P5.AvgPrice) dt
 WHERE dt.OrderRank = 1 
-
-
 
 --9.      List all cities that have never ordered something but we have employees there.
 --
@@ -133,7 +149,7 @@ ORDER BY COUNT(O.OrderID) DESC) T1
 INNER JOIN 
 -- city with most products ordered
 (SELECT TOP 1 C.City
-FROM Customers C INNER JOIN Orders O ON O.CustomerID = C.CustomerID INNER JOIN [Order Details] OD ON OD.OrderID = O.OrderID
+FROM Customers C LEFT JOIN Orders O ON O.CustomerID = C.CustomerID INNER JOIN [Order Details] OD ON OD.OrderID = O.OrderID
 GROUP BY C.City
 ORDER BY SUM(OD.Quantity) DESC) T2 
 
@@ -144,7 +160,8 @@ ON T1.City = T2.City;
 
 
 /*
-we can use distinct * to get the table without duplicates. 
+we can use distinct * to get the table without duplicates and replace the old table with the new 
+table without duplicates. 
 
-we can also use group by or row_number to find duplicates and then delete the duplicates.
+we can also use group by or row_number to find duplicates and then delete the duplicate rows.
 */ 
